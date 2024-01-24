2b8703cf18741bd26df27d5dffa06ff45eeaa4b1
Kanushka Gayan <kanushkanet@gmail.com>
2024-01-24 11:56:14 +0530

    Add machine learning model for sleep score prediction

--- app/lib/custom_code/actions/predict_sleep_score_action.dart
+++ app/lib/custom_code/actions/predict_sleep_score_action.dart
@@ -1,15 +1,77 @@
-// Automatic FlutterFlow imports
 import '/backend/backend.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import 'index.dart'; // Imports other custom actions
 import '/flutter_flow/custom_functions.dart'; // Imports custom functions
 import 'package:flutter/material.dart';
-// Begin custom action code
-// DO NOT REMOVE OR MODIFY THE CODE ABOVE!
+import 'package:tflite/tflite.dart';
+import 'dart:typed_data';
+import 'package:flutter/services.dart';
 
-// Set your action name, define your arguments and return parameter,
-// and then add the boilerplate code using the green button on the right!
+import 'dart:typed_data';
+import 'package:flutter/services.dart';
+import 'package:tflite/tflite.dart';
+
+// Function to load the personalized model
+Future<String?> loadPersonalizedModel() async {
+  return await Tflite.loadModel(
+    model: "assets/tfl_models/single_user_rf_model.tflite",
+  );
+}
+
+// Function to load the generalized model
+Future<String?> loadGeneralizedModel() async {
+  return await Tflite.loadModel(
+    model: "assets/tfl_models/multi_user_rf_model.tflite",
+  );
+}
+
+// Function to run inference
+Future<dynamic> runInference(List<double> inputList) async {
+  var inputBytes = Float32List.fromList(inputList).buffer.asUint8List();
+  var output = await Tflite.runModelOnBinary(binary: inputBytes);
+  return output;
+}
+
+// Calculate the weights based on the R^2 scores
+List<double> calculateWeights(double r2General, double r2Personal) {
+  double wg = r2General / (r2General + r2Personal);
+  double wp = r2Personal / (r2General + r2Personal);
+  return [wg, wp];
+}
+
+// Calculate the unified sleep quality score
+double calculateUnifiedScore(double sGeneral, double sPersonal, double wg, double wp) {
+  double sUnified = wg * sGeneral + wp * sPersonal;
+  return sUnified;
+}
+
+// Unified score calculation utility function
+Future<double> calculateUnifiedScoreFromFeatures(List<double> inputFeatures) async {
+  // Load and run inference on the personalized model
+  await loadPersonalizedModel();
+  var predictionPersonalizedModel = await runInference(inputFeatures);
+  Tflite.close();
+
+  // Load and run inference on the generalized model
+  await loadGeneralizedModel();
+  var predictionGeneralizedModel = await runInference(inputFeatures);
+  Tflite.close();
+
+  //  R² scores 
+  double r2General = 0.948;
+  double r2Personal = 0.986;
+
+  // Calculate the weights based on the R² scores
+  List<double> weights = calculateWeights(r2General, r2Personal);
+  double wg = weights[0];
+  double wp = weights[1];
+
+  // Calculate the unified sleep quality score
+  double sUnified = calculateUnifiedScore(predictionGeneralizedModel, predictionPersonalizedModel, wg, wp);
+
+  return sUnified;
+}
 
 Future<double> predictSleepScoreAction(
   List<StepsRecord>? stepList,
@@ -66,12 +128,15 @@ Future<double> predictSleepScoreAction(
       ? totalBedInTime / todaySleepRecords.length
       : 0;
 
-  // TODO: use machine learning model to predict value
-  // HACK: create fake value for testing
-  final score = ((totalSteps / 10000) * 0.1 +
-      (totalCalories / 400) * 0.2 +
-      (totalDistance / 5000) * 0.1 +
-      (avgBedInTime / 420) * 0.4);
+  List<double> inputList = [
+    totalSteps.toDouble(),
+    totalCalories.toDouble(),
+    totalDistance.toDouble(),
+    avgBedInTime.toDouble()
+  ]; 
 
-  return score > 10 ? score / 100 : 0;
+  // Calculate unified score from features
+  double unifiedScore = await calculateUnifiedScoreFromFeatures(inputList);
+
+  return unifiedScore;
 }
